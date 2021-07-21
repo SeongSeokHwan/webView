ea4fbafbe7879cc44968a7110145bfc0a6be8a7a
SeongSeokHwan <tjd458@naver.com>
2021-07-21 17:49:23 +0900

    commit

---
+++ app/src/main/java/com/example/webview/Fragment_4.java
@@ -0,0 +1,46 @@
+package com.example.webview;
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.KeyEvent;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.webkit.WebSettings;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+
+public class Fragment_4 extends Fragment {
+    WebView mWebView;
+    WebSettings mWebSettings;
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        View view = inflater.inflate(R.layout.fragment__4, container, false);
+        WebView webView = view.findViewById(R.id.web4);
+        webView.getSettings().setJavaScriptEnabled(true);
+        webView.setWebViewClient(new WebViewClient());
+        webView.loadUrl("https://github.com/");
+        webView.setOnKeyListener(new View.OnKeyListener() {
+            @Override
+            public boolean onKey(View v, int keyCode, KeyEvent event) {
+                if (event.getAction() == KeyEvent.ACTION_DOWN){
+                    if (keyCode == KeyEvent.KEYCODE_BACK){
+                        if (webView!=null){
+                            if (webView.canGoBack()){
+                                webView.goBack();
+                            }else {
+                                getActivity().onBackPressed();
+                            }
+                        }
+                    }
+                }return true;
+            }
+        });
+        return view;
+    }
+}
--- app/src/main/java/com/example/webview/MainActivity.java
+++ app/src/main/java/com/example/webview/MainActivity.java
@@ -1,35 +1,53 @@
 package com.example.webview;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.webkit.WebSettings;
 import android.webkit.WebView;
 import android.webkit.WebViewClient;
 
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+
 public class MainActivity extends AppCompatActivity {
-    private WebView mWebView;
-    private WebSettings mWebSettings;
+    BottomNavigationView bottomNavigationView; //바텀 네비게이션 뷰
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        mWebView = (WebView) findViewById(R.id.webview);
+        bottomNavigationView = findViewById(R.id.bottom_nav);
 
-        mWebView.setWebViewClient(new WebViewClient());
-        mWebSettings = mWebView.getSettings(); //각종 환경 설정 가능여부
-        mWebSettings.setJavaScriptEnabled(true); // 자바스크립트 허용여부
-        mWebSettings.setSupportMultipleWindows(false); // 윈도우 여러개 사용여부
-        mWebSettings.setLayoutAlgorithm(WebSettings.LayoutAlgorithm.SINGLE_COLUMN); // 컨텐츠사이즈 맞추기
-        mWebSettings.setCacheMode(WebSettings.LOAD_CACHE_ELSE_NETWORK); // 캐시 허용 여부
-        mWebSettings.setUseWideViewPort(true); // wide viewport 사용 여부
-        mWebSettings.setSupportZoom(true); // Zoom사용여부
-        mWebSettings.setJavaScriptCanOpenWindowsAutomatically(false); // 자바스크립트가 window.open()사용할수있는지 여부
-        mWebSettings.setLoadWithOverviewMode(true); // 메타태그 허용 여부
-        mWebSettings.setBuiltInZoomControls(false); // 화면 확대 축소 허용 여부
-        mWebSettings.setDomStorageEnabled(true); // 로컬저장소 허용 여부
-        mWebView.loadUrl("https://www.naver.com/"); // 웹뷰 사이트 주소 및 시작
+
+        getSupportFragmentManager().beginTransaction().add(R.id.main_Frame, new Fragment_Home()).commit();
+
+
+        bottomNavigationView.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem menuItem) {
+                switch (menuItem.getItemId()) {
+                    //item을 클릭시 id값을 가져와 FrameLayout에 fragment.xml띄우기
+                    case R.id.page_home:
+                        getSupportFragmentManager().beginTransaction().replace(R.id.main_Frame, new Fragment_Home()).commit();
+                        break;
+                    case R.id.page_scan:
+                        getSupportFragmentManager().beginTransaction().replace(R.id.main_Frame, new Fragment_2()).commit();
+                        break;
+                    case R.id.page_order:
+                        getSupportFragmentManager().beginTransaction().replace(R.id.main_Frame, new Fragment_3()).commit();
+                        break;
+                    case R.id.page_add:
+                        getSupportFragmentManager().beginTransaction().replace(R.id.main_Frame, new Fragment_4()).commit();
+                        break;
+                }
+                return true;
+            }
+        });
     }
 }
