c224b34ad4a0f90bf78dd155782fbb80be22ffb7
SeongSeokHwan <tjd458@naver.com>
2021-07-26 10:38:03 +0900

    commit

--- app/src/main/java/com/example/webview/Fragment_Board.java
+++ app/src/main/java/com/example/webview/Fragment_Board.java
@@ -1,19 +1,38 @@
 package com.example.webview;
 
+import android.os.Binder;
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
 
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.webkit.WebView;
+import android.widget.Button;
+import android.widget.Toast;
 
-public class Fragment_Board extends Fragment {
+public class Fragment_Board extends Fragment implements View.OnClickListener {
 
     @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+    public View onCreateView(@NonNull LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
 
+        View view = inflater.inflate(R.layout.fragment__board, container, false);
 
-        return inflater.inflate(R.layout.fragment__board, container, false);
+        Button button = view.findViewById(R.id.temp_button);
+        button.setOnClickListener(this);
+
+        return view;
+    }
+
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()) {
+            case R.id.temp_button:
+                Log.i("test","test");
+                break;
+        }
     }
 }
